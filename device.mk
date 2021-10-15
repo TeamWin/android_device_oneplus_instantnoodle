54a651851339f8f67f902ad2fbce2aaae023cf75
zzitoz <zzitoz@gmail.com>
2021-10-15 13:25:57 +0100

    beautify: proper identation
    Change-Id: Ic887691732754d12c12bcac811288e4104ee9ebc

--- device.mk
+++ device.mk
@@ -13,24 +13,24 @@ AB_OTA_UPDATER := true
 # with "_a" and "_b" variants in the device. Note that the vendor can add more
 # more partitions to this list for the bootloader and radio.
 AB_OTA_PARTITIONS += \
-    boot \
-    system \
-		system_ext \
-    vendor \
-    vbmeta \
-		dtbo
+	boot \
+	system \
+	system_ext \
+	vendor \
+	vbmeta \
+	dtbo
 
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
+	otapreopt_script \
+	update_engine \
+	update_engine_sideload \
+	update_verifier
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+	RUN_POSTINSTALL_system=true \
+	POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+	FILESYSTEM_TYPE_system=ext4 \
+	POSTINSTALL_OPTIONAL_system=true
 
 # tell update_engine to not change dynamic partition table during updates
 # needed since our qti_dynamic_partitions does not include
@@ -39,36 +39,36 @@ TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl-wrapper.recovery \
-    android.hardware.boot@1.0-impl-wrapper \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.$(PRODUCT_PLATFORM) \
-    bootctrl.$(PRODUCT_PLATFORM).recovery \
+	android.hardware.boot@1.0-impl \
+	android.hardware.boot@1.0-service \
+	android.hardware.boot@1.0-impl-wrapper.recovery \
+	android.hardware.boot@1.0-impl-wrapper \
+	android.hardware.boot@1.0-impl.recovery \
+	bootctrl.$(PRODUCT_PLATFORM) \
+	bootctrl.$(PRODUCT_PLATFORM).recovery
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd \
-    resetprop
+	android.hardware.fastboot@1.0-impl-mock \
+	fastbootd \
+	resetprop
 
 # qcom decryption
 PRODUCT_PACKAGES_ENG += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
+	qcom_decrypt \
+	qcom_decrypt_fbe
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+	$(LOCAL_PATH)
 
 # tzdata
 PRODUCT_PACKAGES_ENG += \
-    tzdata_twrp
+	tzdata_twrp
 
 # Apex libraries
 PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+	$(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
