c36cbe16ac67c5eb9857d6096001b9ed2f34c1d4
infected_ <zzitoz@gmail.com>
2021-12-18 01:27:55 +0000

    add variable "TW_DEVICE_VERSION" for device specific test versions

--- twrp_instantnoodle.mk
+++ twrp_instantnoodle.mk
@@ -29,3 +29,6 @@ PRODUCT_MANUFACTURER := oneplus
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/device.mk)
+
+# device specific twrp version
+TW_DEVICE_VERSION  := op8_b1
