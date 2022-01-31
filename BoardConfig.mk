daaf7f2d37b7321db742fa2552b2c69360a42e12
infectedmushi <zzitoz@gmail.com>
2022-01-31 18:22:34 +0000

    Revert "update RECOVERY_INSTALLER_PATH to proper path"
    This reverts commit 08839ed83ee405f921630101b7ced56aba20ce66.
    
    Reason for revert: mixed up the path's, revert is needed
    
    Change-Id: I0a29d28836364ea606ed50c99b63f81512dbc781

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,7 +201,4 @@ TW_EXCLUDE_ENCRYPTED_BACKUPS := false
 TW_SCREEN_BLANK_ON_BOOT := true
 
 USE_RECOVERY_INSTALLER := true
-RECOVERY_INSTALLER_PATH := $(TOP)/external/magisk-prebuilt
-
-
-
+RECOVERY_INSTALLER_PATH := /mnt/Building/template_twrp
